f4be2648511e72f8e17aad7e855036acf5196b67
jalsayson <jlsayson@up.edu.ph>
2019-03-11 21:25:24 +0800

    Initial word-level prediction implementation added

--- Implementation/SP/js/popup.js
+++ Implementation/SP/js/popup.js
@@ -1,101 +1,3 @@
-class PTNode {
-    constructor(value){
-        this.value = value;
-        this.final = 0;
-        this.children = {};
-    }
-
-    hasChild(value){
-        var keys = Object.keys(this.children);
-        for(var key of keys) {
-            if(value == key) return true;
-        }
-        return false;
-    }
-
-    hasChildren(){
-        return Object.keys(this.children).length;
-    }
-
-    getChildren(){
-        return Object.keys(this.children);
-    }
-}
-
-class PrefixTree{
-    constructor(){
-        this.head = new PTNode(null);
-    }
-
-    insert(word){
-        var insert_helper = function(nav, word){
-            var keys = Object.keys(nav.children);
-            var check = true;
-            for(var key of keys){
-                if(key == word[0]){
-                    check = false;
-                    break;
-                }
-            }
-            if(check){
-                //insert node
-                nav.children[word[0]] = new PTNode(word[0]);
-            }
-            if(word.slice(1).length == 0) nav.children[word[0]].final = 1;
-            if(word.slice(1).length != 0) insert_helper(nav.children[word[0]], word.slice(1));
-        }
-        insert_helper(this.head, word);
-    }
-
-    print_all(){
-        var print_helper = function(nav, word){
-            if(nav.final){
-                console.log(word+nav.value);
-            }
-            var keys = Object.keys(nav.children);
-            for(var key of keys){
-                print_helper(nav.children[key], (nav.value != null?word+nav.value:word));
-            }
-        }
-        print_helper(this.head, "");
-    }
-
-    word_search(term){
-        var print_helper = function(nav, word){
-            if(nav.final){
-                // console.log(word+nav.value);
-                results = results.concat(word+nav.value);
-            }
-            if(!nav.hasChildren()) return;
-            for(var key of nav.getChildren()){
-                print_helper(nav.children[key], (nav.value != null?word+nav.value:word));
-            }
-        }
-        var nav = this.head;
-        var src = term;
-        var results = [];
-
-        if(term == "") {
-            print_helper(nav, "", results);
-        }
-
-        else{
-            while(src.length > 1){
-                if(nav.hasChild(src[0])){
-                    nav = nav.children[src[0]];
-                    src = src.slice(1);
-                }
-                else break;
-            }
-            if(!nav.hasChild(src[0])) return "empty";
-            print_helper(nav.children[src[0]], term.slice(0,term.length-1), results);
-        }
-
-        return results;
-    }
-
-}
-
 class NameProbabilityPair {
     constructor(name, value) {
         this.name = name;
@@ -110,119 +12,132 @@ class NameProbabilityPair {
     }
 }
 
-const npp_compare = function(a, b){
-    if(a.value > b.value) return -1;
-    if(a.value < b.value) return 1;
-    return 0;
-}
+class PredictionMatrix {
+    constructor(bag, sequence){
+        this.matrix = {};
+        this.reference = {
+            'bag' : bag.getBag(),
+            'sequence' : sequence,
+            'keys' : bag.keys()
+        };
 
-const hasProperty = function(object, property){
-    return Object.prototype.hasOwnProperty.call(object, property);
-}
-
-//source: https://toddmotto.com/methods-to-determine-if-an-object-has-a-given-property/
-
-const clean_text = function(input){
-    //not properly working
-    var copy = input;
-    var regex1 = /[^\w\s]/g;
-    var regex2 = /[\n]/g;
-    // var regex2 = /\s{2,}/g;
-    var replaced = copy.replace(regex1, "").replace(regex2, "\\n");
-    var lines = replaced.toLowerCase().split("\n");
-    return lines;
-}
-//other regex: .,\/#!$%\^&\*;:{}=\-_`~()
-//other regex src: https://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
-
-const create_bag = function(input){
-    var bag = {};
-    var copy = input;
-
-    for(var i = 0; i < input.length; i++) {
-        text = input[i].split(" ");
-        for (var j = 0; j < text.length; j++){
-            if(!hasProperty(bag, text[j])){
-                bag[text[j]] = 0;
+        var keys = this.reference['keys'];
+        for(var key1 of keys){
+            this.matrix[key1] = {};
+            for(var key2 of keys){
+                this.matrix[key1][key2] = 0;
             }
-            bag[text[j]]++;
         }
-    }
-    return bag;
-}
 
-const print_bag = function(bag){
-    var keyset = Object.keys(bag)
-    for(var i = 0; i < keyset.length; i++){
-        console.log(keyset[i] + " : " + bag[keyset[i]])
-    }
-}
-
-const convert_to_chain = function(clean_text){
-    var word_list = []
-    for(var i = 0; i < clean_text.length; i++){
-        var text = clean_text[i].split(" ");
-        word_list = word_list.concat(text);
-    }
-    return word_list;
-}
-
-const occurence_count = function(matrix, chain){
-    // console.log(chain)
-    for (var i = 1; i < chain.length; i++){
-        matrix[chain[i]][chain[i-1]]+=1;
-    }
-}
-
-const divide_by_occurences = function(matrix, bag){
-    var keyset = Object.keys(matrix);
-    for(var i = 0; i < keyset.length; i++){
-        for(var j = 0; j < keyset.length; j++){
-            matrix[keyset[i]][keyset[j]] = matrix[keyset[i]][keyset[j]]/bag[keyset[i]];
+        // console.log(Object.keys(this.matrix));
+        for(var i = 1; i < sequence.length; i++){
+            this.matrix[sequence[i]][sequence[i-1]]++;
         }
-    }
-    return matrix;
-}
 
-const gen_prob_matrix = function(bag, chain){
-    var matrix = {}
-    var keyset = Object.keys(bag);
-    for(var i = 0; i < keyset.length; i++){
-        matrix[keyset[i]] = {}
-        for(var j = 0; j < keyset.length; j++){
-            matrix[keyset[i]][keyset[j]] = 0;
+        for(var key1 of keys){
+            for(var key2 of keys){
+                this.matrix[key1][key2] = this.matrix[key1][key2]/this.reference['bag'][key1];
+            }
         }
     }
 
-    occurence_count(matrix, chain);
-    matrix = divide_by_occurences(matrix, bag);
+    printMatrix(){
+        for(var key of Object.keys(this.matrix)){
+            for(var key2 of Object.keys(this.matrix[key])){
+                console.log(key + " given " + key2 + " : " + this.matrix[key][key2]);
+            }
+        }
+    }
 
-    return matrix;
+    getTopRanks(word, ranks = 3){
+        var ranking = [];
+        var npp_compare = function(a, b){
+            if(a.value > b.value) return -1;
+            if(a.value < b.value) return 1;
+            return 0;
+        }
+        var keys = this.reference['keys'];
+        for(var key of keys){
+            var converted_pair = this.bayesComputation(word, key);
+            ranking = ranking.concat(new NameProbabilityPair(key, converted_pair));
+            ranking.sort(npp_compare);
+            if(ranking.length > ranks) ranking = ranking.slice(0, ranks);
+        }
+        return ranking;
+    }
+
+    aGivenB(a,b){
+        return this.matrix[a][b];
+    }
+
+    probOf(word){
+        return this.reference['bag'][word]/this.reference['sequence'].length;
+    }
+
+    bayesComputation(word, key){
+        if(this.probOf(key) == 0 || this.aGivenB(word, key) == 0) return 0;
+        else return (this.aGivenB(word, key)*this.probOf(key))/this.probOf(word);
+    }
+
 }
 
-const print_prob_matrix = function(matrix){
-    var keyset = Object.keys(matrix);
-    for(var i = 0; i < keyset.length; i++){
-        for(var j = 0; j < keyset.length; j++){
-            console.log(keyset[i] + " given " + keyset[j] + " = " + matrix[keyset[i]][keyset[j]]);
+class WordBag {
+    constructor(sequence){
+        this.bag = {};
+        if(typeof sequence !== undefined){
+            for(var i = 0; i < sequence.length; i++) {
+                this.increment(sequence[i]);
+            }
+        }
+    }
+
+    hasKey(value){
+        var keys = Object.keys(this.bag);
+        for(var key of keys) {
+            if(value == key) return true;
+        }
+        return false;
+    }
+
+    increment(name){
+        if(!this.hasKey(name)) this.bag[name] = 0;
+        this.bag[name]++;
+    }
+
+    insert(name, count){
+        this.bag[name] = count;
+    }
+
+    getCount(name){
+        return this.bag[name];
+    }
+
+    getBag(){
+        return this.bag;
+    }
+
+    keys(){
+        return Object.keys(this.bag);
+    }
+
+    printBag(){
+        var keyset = Object.keys(this.bag);
+        for(var i = 0; i < keyset.length; i++){
+            console.log(keyset[i] + " : " + bag[keyset[i]])
         }
     }
 }
 
-const get_top_rankers = function(word, slots, matrix, chain, bag){
-    var ranking = []
-    var p_word = bag[word]/text_chain.length;
-    var keyset = Object.keys(matrix[word]);
-    for(var i = 0; i < keyset.length; i++){
-        var p_key2 = bag[keyset[i]]/text_chain.length;
-        var p_word_key2 = matrix[keyset[i]][word];
-        if(p_key2 == 0 || p_word_key2 == 0) continue;
-        var converted_pair = new NameProbabilityPair(keyset[i], ((p_word_key2*p_key2)/p_word))
-        ranking = ranking.concat([converted_pair])
+const createSequence = function(source){
+    var sequence = [];
+    var lines = source.split("\n");
+    for(var i = 0; i < lines.length; i++) {
+        var text = lines[i].split(" ");
+        for (var j = 0; j < text.length; j++){
+            sequence = sequence.concat([text[j]]);
+        }
     }
-    ranking.sort(npp_compare);
-    //sort ranking
-    return ranking;
+    return sequence;
 }
 
 const initial_processing = function(){
@@ -277,6 +192,31 @@ const text_processing = function(){
     print_prob_matrix(probabilities)
 }
 
+const create_button = function(option){
+    var div = document.createElement("div");
+    div.className = "three column";
+    div.innerHTML = option;]
+    return div;
+}
+
+const test_matrix = function(){
+    var string = document.getElementById('text-space').value;
+    var seq = createSequence(string);
+    var bag = new WordBag(seq);
+    // console.log(seq);
+    var pdm = new PredictionMatrix(bag, seq);
+    // pdm.printMatrix();
+    var rankers = pdm.getTopRanks("i", 10);
+    // var insert = [];
+    var cont = document.getElementById('choices');
+    while (cont.firstChild) {
+        cont.removeChild(cont.firstChild);
+    }
+    for(var i = 0; i < 10; i++){
+        cont.appendChild(create_button(rankers[i].name));
+    }
+}
+
 const on_run = function(){
     // var src = document.getElementById("copy-button");
     // src.addEventListener("click", text_processing);
@@ -286,7 +226,7 @@ const on_run = function(){
     src2.addEventListener ("click", caret_sens_text_check);
 
     var src3 = document.getElementById("begin-button");
-    src3.addEventListener("click", initial_processing);
+    src3.addEventListener("click", test_matrix);
 }
 
 // let current = "";
---
+++ Implementation/SP/js/word_network.js
@@ -0,0 +1,234 @@
+/*
+DEVELOPMENT LINEUP:
+    - regex fix
+    - connecting code to frontend
+    - data saving to chrome storage
+    - option/settings page
+    - dynamic option count (determine max option)
+*/
+
+class NameProbabilityPair {
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    compare(comp) {
+        if(this.value > comp.value) {
+            return this
+        }
+        return comp;
+    }
+}
+
+
+class NetworkNode {
+    constructor(word) {
+        this.word = word;
+        this.end = 0;
+        this.children = {};
+        this.count = 0;
+    }
+
+    hasChild(value){
+        var keys = Object.keys(this.children);
+        for(var key of keys) {
+            if(value == key) return true;
+        }
+        return false;
+    }
+
+    hasChildren(){
+        return Object.keys(this.children).length;
+    }
+
+    getChildren(){
+        return Object.keys(this.children);
+    }
+
+    getCount(){
+        return this.count;
+    }
+
+    increment(){
+        this.count++;
+    }
+}
+
+/*
+class WordNetwork {
+    constructor(){
+        this.head = new NetworkNode(null);
+    }
+
+    cleanText(input){
+        //not properly working
+        var copy = input;
+        var regex1 = /[^\w\s]/g;
+        var regex2 = /[\n]/g;
+        // var regex2 = /\s{2,}/g;
+        var replaced = copy.replace(regex1, "").replace(regex2, "\\n");
+        var lines = replaced.toLowerCase().split("\n");
+        return lines;
+    }
+
+    insert(sequence){
+        var insert_helper = function(nav, sequence){
+            if(!nav.hasChild(sequence[0])){
+                //insert node
+                nav.children[sequence[0]] = new NetworkNode(sequence[0]);
+            }
+            console.log(sequence.slice(1));
+            if(sequence.slice(1).length == 0) nav.children[sequence[0]].end = 1;
+            if(sequence.slice(1).length != 0) insert_helper(nav.children[sequence[0]], sequence.slice(1));
+        }
+        insert_helper(this.head, sequence);
+    }
+
+    print(){
+        var print_helper = function(nav, word){
+            if(nav.word != null) console.log(word+" "+nav.word);
+            for(var key of nav.getChildren()){
+                print_helper(nav.children[key], (nav.word != null?word+" "+nav.word:word));
+            }
+        }
+        print_helper(this.head, "");
+    }
+}
+*/
+
+class PredictionMatrix {
+    constructor(bag, sequence){
+        this.matrix = {};
+        this.reference = {
+            'bag' : bag.getBag(),
+            'sequence' : sequence,
+            'keys' : bag.keys()
+        };
+
+        var keys = this.reference['keys'];
+        for(var key1 of keys){
+            this.matrix[key1] = {};
+            for(var key2 of keys){
+                this.matrix[key1][key2] = 0;
+            }
+        }
+
+        // console.log(Object.keys(this.matrix));
+        for(var i = 1; i < sequence.length; i++){
+            this.matrix[sequence[i]][sequence[i-1]]++;
+        }
+
+        for(var key1 of keys){
+            for(var key2 of keys){
+                this.matrix[key1][key2] = this.matrix[key1][key2]/this.reference['bag'][key1];
+            }
+        }
+    }
+
+    printMatrix(){
+        for(var key of Object.keys(this.matrix)){
+            for(var key2 of Object.keys(this.matrix[key])){
+                console.log(this.matrix[key][key2]);
+            }
+        }
+    }
+
+    getTopRanks(word, ranks = 3){
+        var ranking = [];
+        var npp_compare = function(a, b){
+            if(a.value > b.value) return -1;
+            if(a.value < b.value) return 1;
+            return 0;
+        }
+        var keys = this.reference['keys'];
+        for(var key of keys){
+            var converted_pair = this.bayesComputation(word, key);
+            ranking = ranking.concat(new NameProbabilityPair(key, converted_pair));
+            ranking.sort(npp_compare);
+            if(ranking.length > ranks) ranking = ranking.slice(0, ranks);
+        }
+        return ranking;
+    }
+
+    aGivenB(a,b){
+        return this.matrix[a][b];
+    }
+
+    probOf(word){
+        return this.reference['bag'][word]/this.reference['sequence'].length;
+    }
+
+    bayesComputation(word, key){
+        if(this.probOf(key) == 0 || this.aGivenB(word, key) == 0) return 0;
+        else return (this.aGivenB(word, key)*this.probOf(key))/this.probOf(word);
+    }
+
+}
+
+class WordBag {
+    constructor(sequence){
+        this.bag = {};
+        if(typeof sequence !== undefined){
+            for(var i = 0; i < sequence.length; i++) {
+                this.increment(sequence[i]);
+            }
+        }
+    }
+
+    hasKey(value){
+        var keys = Object.keys(this.bag);
+        for(var key of keys) {
+            if(value == key) return true;
+        }
+        return false;
+    }
+
+    increment(name){
+        if(!this.hasKey(name)) this.bag[name] = 0;
+        this.bag[name]++;
+    }
+
+    insert(name, count){
+        this.bag[name] = count;
+    }
+
+    getCount(name){
+        return this.bag[name];
+    }
+
+    getBag(){
+        return this.bag;
+    }
+
+    keys(){
+        return Object.keys(this.bag);
+    }
+
+    printBag(){
+        var keyset = Object.keys(this.bag);
+        for(var i = 0; i < keyset.length; i++){
+            console.log(keyset[i] + " : " + bag[keyset[i]])
+        }
+    }
+}
+
+const createSequence = function(source){
+    var sequence = [];
+    var lines = source.split("\n");
+    for(var i = 0; i < lines.length; i++) {
+        var text = lines[i].split(" ");
+        for (var j = 0; j < text.length; j++){
+            sequence = sequence.concat([text[j]]);
+        }
+    }
+    return sequence;
+}
+var string = "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn't think they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley pretended she didn't have a sister, because her sister and her good-for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. This boy was another good reason for keeping the Potters away; they didn't want Dudley mixing with a child like that. When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing about the cloudy sky outside to suggest that strange and mysterious things would soon be happening all over the country. Mr. Dursley hummed as he picked out his most boring tie for work, and Mrs. Dursley gossiped away happily as she wrestled a screaming Dudley into his high chair. None of them noticed a large, tawny owl flutter past the window. At half past eight, Mr. Dursley picked up his briefcase, pecked Mrs. Dursley on the cheek, and tried to kiss Dudley good-bye but missed, because Dudley was now having a tantrum and throwing his cereal at the walls. 'Little tyke,' chortled Mr. Dursley as he left the house. He got into his car and backed out of number four's drive. It was on the corner of the street that he noticed the first sign of something peculiar -- a cat reading a map. For a second, Mr. Dursley didn't realize what he had seen -- then he jerked his head around to look again. There was a tabby cat standing on the corner of Privet Drive, but there wasn't a map in sight. What could he have been thinking of? It must have been a trick of the light. Mr. Dursley blinked and stared at the cat. It stared back. As Mr. Dursley drove around the corner and up the road, he watched the cat in his mirror. It was now reading the sign that said Privet Drive -- no, looking at the sign; cats couldn't read maps or signs. Mr. Dursley gave himself a little shake and put the cat out of his mind. As he drove toward town he thought of nothing except a large order of drills he was hoping to get that day.";
+
+var seq = createSequence(string);
+var bag = new WordBag(seq);
+// console.log(seq);
+var pdm = new PredictionMatrix(bag, seq);
+// pdm.printMatrix();
+console.log(pdm.getTopRanks("he", 10));
